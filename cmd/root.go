a0fdc63448b51182e04c0b7736efe422b0d65bff
Gabriel Bazzotti <gabriel.bazzotti@suse.com>
2025-11-20 16:14:39 -0300

    feat(cmd): add beginning logic select flags for rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,37 @@
 package cmd
 
 import (
-	"os"
-
+	"fmt"
 	"github.com/spf13/cobra"
+	"os"
 )
+
+// Flag declaration
+var flagPolicy string
+var flagRegion string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "opencpe",
 	Short: "All-in-one Cloud-Native Policy Engine with sensible defaults",
 	Long: `OpenCPE is a tool for managing cloud resources with heavily opinionated defaults.
+
+Note: All Global flags are required
+	
 A comprehensive policy reference and usage instructions can be found at https://github.com/bazgab/opencpe
 `,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		// In case there are no flags being passed, return the help page
+		if cmd.Flags().NFlag() == 0 {
+			cmd.Help()
+			return
+		}
+
+		fmt.Println("Initializing: ")
+		fmt.Printf("Policy: %s\n", flagPolicy)
+		fmt.Printf("Region: %s\n", flagRegion)
+		// You can call another function here, e.g., defaultRunLogic()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -31,10 +47,20 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&flagRegion, "region", "", "Region to be queried (default is $OPENCPE_DEFAULT_REGION)")
+	rootCmd.PersistentFlags().StringVar(&flagPolicy, "policy", "", "Policy to be executed")
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.opencpe.yaml)")
+	err := rootCmd.MarkPersistentFlagRequired("region")
+	if err != nil {
+		panic(err)
+	}
+
+	err = rootCmd.MarkPersistentFlagRequired("policy")
+	if err != nil {
+		panic(err)
+	}
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
 }
