f806e09acc9656e5e049d4bc2a8fe0e1dca890c8
Gabriel Bazzotti <gabriel.bazzotti@suse.com>
2025-12-01 21:58:52 -0300

    feat(cmd): restruct JSONMarshaller for ease of access to string slices

--- cmd/notify.go
+++ cmd/notify.go
@@ -1,31 +1,30 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/bazgab/opencpe/utils/logging"
 	"github.com/spf13/cobra"
+	"log"
+	"os"
 )
 
 type Config struct {
-	Authentication Authentication `json:"authentication"`
-	Notification   Notification   `json:"notification"`
-	IgnoredTags    IgnoredTags    `json:"ignored_tags"`
-}
+	Authentication struct {
+		AwsProfile string `json:"aws_profile"`
+	} `json:"authentication"`
 
-type Authentication struct {
-	AwsProfile string `json:"aws_profile"`
-}
+	Notification struct {
+		SmtpHost      string `json:"smtp_host"`
+		SmtpPort      int    `json:"smtp_port"`
+		EmailFrom     string `json:"email_from"`
+		EmailPassword string `json:"email_password"`
+	} `json:"notification"`
 
-type Notification struct {
-	SmtpHost      string `json:"smtp_host"`
-	SmtpPort      int    `json:"smtp_port"`
-	EmailFrom     string `json:"email_from"`
-	EmailPassword string `json:"email_password"`
-}
-
-type IgnoredTags struct {
-	Owner   string `json:"owner"`
-	Project string `json:"project"`
+	IgnoredTags struct {
+		Owner   []string `json:"owner"`
+		Project []string `json:"project"`
+	} `json:"ignored_tags"`
 }
 
 var notifyCmd = &cobra.Command{
@@ -47,6 +46,30 @@ var notifyCmd = &cobra.Command{
 		// Checking if global flags are working
 		fmt.Println("Query Request Output:")
 		logging.TextRequestOutputLogger("Printing values from global flags", flagConfig, flagPolicy, flagRegion)
+
+		cfgFile, err := os.ReadFile(flagConfig)
+		if err != nil {
+			log.Fatal("Error when opening file: ", err)
+		}
+
+		var config Config
+
+		err = json.Unmarshal(cfgFile, &config)
+		if err != nil {
+			log.Fatal("Error during Unmarshal(): ", err)
+		}
+
+		fmt.Println("Loaded Configuration:")
+		fmt.Printf("-- Authentication.Profile: %s\n", config.Authentication.AwsProfile)
+		fmt.Printf("-- Notification.SMTP Host: %s\n", config.Notification.SmtpHost)
+		fmt.Printf("-- Notification.SMTP Port: %d\n", config.Notification.SmtpPort)
+		fmt.Printf("-- Notification.From Email: %s\n", config.Notification.EmailFrom)
+		for _, owner := range config.IgnoredTags.Owner {
+			fmt.Printf("-- IgnoredTags.Owner: %s\n", owner)
+		}
+		for _, project := range config.IgnoredTags.Project {
+			fmt.Printf("-- IgnoredTags.Project: %s\n", project)
+		}
 
 	},
 }
